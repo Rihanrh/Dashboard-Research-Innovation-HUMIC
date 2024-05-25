249f6e8f774586b8223184e8acec4b152ceb272d
Rihanrh <rihanh@student.telkomuniversity.ac.id>
2024-05-25 14:29:02 +0700

    Updated app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
+from streamlit_navigation_bar import st_navbar
+
+# Create the navigation bar
+page = st_navbar(["Home", "(PEROLEHAN)Hibah Penelitian", "(PENGUSULAN)Hibah Penelitian", "(PEROLEHAN)Publikasi Ilmiah", "(PENGUSULAN)Publikasi Ilmiah", "(PEROLEHAN)HKI", "(PENGUSULAN)HKI", "(PEROLEHAN)Hibah Pengmas", "(PENGUSULAN)Hibah Pengmas", "(PEROLEHAN)Hibah Industri", "(PENGUSULAN)Hibah Industri"])
+st.write(page)
 
 # Read the Excel file
 df = pd.read_excel('Data HUMIC 2024.xlsx')
--- home.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from streamlit_navigation_bar import st_navbar
-import pandas as pd
-
-page = st_navbar(["Home", "(PEROLEHAN)Hibah Penelitian", "(PENGUSULAN)Hibah Penelitian", "(PEROLEHAN)Publikasi Ilmiah", "(PENGUSULAN)Publikasi Ilmiah", "(PEROLEHAN)HKI", "(PENGUSULAN)HKI", "(PEROLEHAN)Hibah Pengmas", "(PENGUSULAN)Hibah Pengmas", "(PEROLEHAN)Hibah Industri", "(PENGUSULAN)Hibah Industri"])
-st.write(page)
